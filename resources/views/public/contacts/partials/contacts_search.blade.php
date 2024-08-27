6d2f1940a7ae5510b6a7f46dbf76fb89c1f6eaac
Matheus André Bezerra Mota <matheus.mota@saudecaruaru.local>
2024-08-27 12:32:32 -0300

    InfoSaúdeCaruaru_V1.0_Inclusão_Filtro

--- resources/views/public/contacts/partials/contacts_search.blade.php
+++ resources/views/public/contacts/partials/contacts_search.blade.php
@@ -1,17 +1,5 @@
 <!-- Search -->
 <x-search.formSearch>
-    <x-search.selectSearch label="Unidade" name="searchName">
-        @foreach ($dbEstablishments as $dbEstablishment)
-            <option 
-                value="{{$dbEstablishment->title}}"
-                @isset($search['searchName'])
-                    @if($dbEstablishment->title == $search['searchName']) selected @endif
-                @endisset
-            >
-                {{$dbEstablishment->title}}
-            </option>
-        @endforeach
-    </x-search.selectSearch>
     <x-search.selectSearch label="Ramais" name="searchExtension">
         @foreach ($dbContacts as $dbContact)
             <option 

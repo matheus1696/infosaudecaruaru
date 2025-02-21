c6d2253a94be98e5d8846af82d75b27f7e89615b
Matheus Mota <matheus.mat1996@gmail.com>
2025-02-20 21:00:30 -0300

    Extras

--- resources/views/extra/index.blade.php
+++ resources/views/extra/index.blade.php
@@ -8,27 +8,27 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
-<body class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-green-400 to-green-600 px-4">
+<body class="bg-gradient-to-r from-[#42B029] to-[#004B43] min-h-screen flex flex-col items-center justify-center px-4 overflow-hidden">
     <!-- Logo -->
     <div class="flex justify-center mb-10">
         <img src="https://infosaude.saudecaruaru.pe.gov.br/assets/img/logo_sms_mono_branco.png" alt="Prefeitura de Caruaru" class="w-96 h-auto">
     </div>   
     
     <div class="w-full max-w-sm p-8 bg-white rounded-2xl shadow-xl transition-all duration-500 transform">
-        <h1 class="text-2xl font-bold text-center text-green-700 mb-2">Cadastro de Intensão </h1>
-        <h2 class="text-lg font-medium text-center text-green-600">Plantão Extra</h2>
-        <h2 class="text-lg font-medium text-center text-green-600 mb-4">Secretaria de Saúde de Caruaru</h2>
+        <h1 class="text-2xl font-bold text-center text-[#004B43] mb-2">Cadastro de Intensão </h1>
+        <h2 class="text-lg font-medium text-center text-[#42B029]">Plantão Extra</h2>
+        <h2 class="text-lg font-medium text-center text-[#42B029] mb-4">Secretaria de Saúde de Caruaru</h2>
 
-        <p class="text-center text-gray-600 mb-6 text-sm">
-            Este cadastro é exclusivo para servidores da Secretaria de Saúde de Caruaru que estão ativos.
+        <p class="text-justify text-gray-600 mb-6 text-sm px-2 font-medium">
+            Este cadastro é exclusivo para servidores ativos da Secretaria de Saúde de Caruaru, conforme o Parágrafo Único do Art. 2º da Lei nº 6.915/2022, que regulamenta o pagamento de plantões extraordinários e eventos extras no município.
         </p>
         
         <form action="{{ route('extras.store') }}" method="POST" x-data="{ loading: false }" @submit="loading = true">
             @csrf
             <div class="mb-4">
-                <label class="pl-2 block text-green-700 font-medium mb-1" for="cpf">CPF:</label>
+                <label class="pl-2 block text-[#42B029] font-medium mb-1" for="cpf">CPF:</label>
                 <input type="text" name="cpf" id="cpf" placeholder="Digite seu CPF" 
-                    class="w-full p-2 border border-green-300 rounded-xl focus:outline-none transition-all duration-300 shadow-sm @error('cpf') border-red-500 ring-red-500 @enderror" 
+                    class="w-full p-2 border border-[#42B029] focus:border-[#004B43] rounded-lg outline-none transition-all duration-300 shadow-sm @error('cpf') border-red-500 ring-red-500 @enderror" 
                     maxlength="14" inputmode="numeric" aria-label="Digite seu CPF" autofocus
                     oninput="handleCPF(event)" value="{{ old('cpf') }}">
 
@@ -39,7 +39,7 @@
             </div>
 
             <button type="submit" 
-                class="w-full bg-green-500 text-white font-bold py-3 rounded-xl shadow-lg hover:bg-green-600 transition-all duration-300 flex items-center justify-center active:scale-95 disabled:opacity-50"
+                class="w-full bg-[#42B029] hover:bg-[#004B43] text-white font-bold py-3 rounded-lg shadow-lg  transition-all duration-300 flex items-center justify-center active:scale-95 disabled:opacity-50"
                 :disabled="loading">
                 <span x-show="!loading">Buscar</span>
                 <span x-show="loading" class="animate-spin border-t-2 border-white rounded-full w-5 h-5 ml-2"></span>
--- resources/views/extra/show.blade.php
+++ resources/views/extra/show.blade.php
@@ -10,64 +10,112 @@
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 
-<body
-    class="bg-gradient-to-r from-green-400 to-green-600 min-h-screen flex flex-col items-center justify-start px-4 py-6 mt-10">
+<body class="bg-gradient-to-r from-[#42B029] to-[#004B43] px-4 min-h-screen flex flex-col items-center px-4 py-6 mt-10 overflow-hidden"
+    x-data="{ openModal: false, checked: false }">
+    
     <!-- Logo -->
     <div class="flex justify-center mb-10">
         <img src="https://infosaude.saudecaruaru.pe.gov.br/assets/img/logo_sms_mono_branco.png"
             alt="Prefeitura de Caruaru" class="w-96 h-auto">
     </div>
 
-    <h1 class="text-3xl font-bold text-white mb-1">Cadastro de Intensão</h1>
+    <h1 class="text-3xl font-semibold text-white mb-1">Cadastro de Intenção</h1>
     <h2 class="text-lg font-medium text-center text-white">Plantão Extra</h2>
     <h2 class="text-lg text-center text-white mb-10">Secretaria de Saúde de Caruaru</h2>
-    <h2 class="text-lg text-white mb-2">Informações Profissionais</h2>
 
     <div class="grid grid-cols-1 gap-6 w-full max-w-4xl">
 
         @if ($dbProfessionais->isEmpty())
             <div class="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 rounded-lg shadow-md">
                 <p class="font-semibold">Atenção:</p>
-                <p>Você não está apto para fazer plantão extra no momento.</p>
+                <p>No momento, você não está apto a participar dos plantões extras.</p>
             </div>
         @endif
 
         @foreach ($dbProfessionais as $dbProfessional)
-            <div class="bg-white py-6 px-6 rounded-2xl shadow-lg transition-transform transform">
-                <div class="flex items-center justify-between">
+            <div class="bg-white py-6 px-6 rounded-2xl shadow-lg">
+                <div class="flex flex-col lg:flex-row items-center justify-between gap-4">
                     <div class="flex-1">
-                        <h3 class="text-xl font-semibold text-green-600 mb-1">{{ $dbProfessional->nome }}</h3>
-                        <p class="text-green-500 text-sm mb-1">Matrícula: <span
+                        <h3 class="text-xl font-semibold text-[#004B43] mb-1">{{ $dbProfessional->nome }}</h3>
+                        <p class="text-[#42B029] text-sm mb-1">Matrícula: <span
                                 class="font-semibold">{{ $dbProfessional->matricula }}</span></p>
-                        <p class="text-green-500 text-sm mb-1">CPF: <span
+                        <p class="text-[#42B029] text-sm mb-1">CPF: <span
                                 class="font-semibold">{{ $dbProfessional->cpf }}</span></p>
-                        <p class="text-green-500 text-sm mb-1">Lotação: <span
+                        <p class="text-[#42B029] text-sm mb-1">Lotação: <span
                                 class="font-semibold">{{ $dbProfessional->lotacao }}</span></p>
-                        <p class="text-green-500 text-sm mb-1">Cargo: <span
+                        <p class="text-[#42B029] text-sm mb-1">Cargo: <span
                                 class="font-semibold">{{ $dbProfessional->cargo }}</span></p>
-                        <p class="text-green-500 text-sm">Apto para Extra?
+                        <p class="text-[#42B029] text-sm">Inscrito na lista de plantões extras?
                             <span
-                                class="font-semibold {{ $dbProfessional->apto_extra ? 'text-green-600' : 'text-red-600' }}">
+                                class="font-semibold {{ $dbProfessional->apto_extra ? 'text-[#004B43]' : 'text-red-600' }}">
                                 {{ $dbProfessional->apto_extra ? 'Sim' : 'Não' }}
                             </span>
                         </p>
                     </div>
                     <div>
-                        <a href="{{ route('extras.edit', $dbProfessional->id) }}"
-                            class="{{ $dbProfessional->apto_extra ? 'bg-red-500 hover:bg-red-600' : 'bg-green-500 hover:bg-green-600' }} text-white font-bold py-2 px-4 rounded-lg shadow-md transition duration-300">
-                            {{ $dbProfessional->apto_extra ? 'Sair da Lista' : 'Entrar na Lista' }}
-                        </a>
+                        @if ($dbProfessional->apto_extra)
+                            <a href="{{ route('extras.edit', $dbProfessional->id) }}" class="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300">
+                                Sair da Lista
+                            </a>
+                        @else
+                            <!-- Botão que abre o modal -->
+                            <button @click="openModal = true"
+                                class="bg-[#42B029] hover:bg-[#004B43] text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300">
+                                Entrar na Lista
+                            </button>
+                        @endif
+
                     </div>
                 </div>
             </div>
         @endforeach
     </div>
 
+    <!-- Botão Voltar -->
     <div class="mt-10">
         <a href="{{ route('extras.index') }}"
-            class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-lg shadow-md transition duration-300">
+            class="bg-gray-100 hover:bg-gray-400 text-black hover:text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300">
             Voltar
         </a>
+    </div>
+
+    <!-- Modal -->
+    <div>
+        <div x-show="openModal" class="fixed inset-0 bg-black bg-opacity-[.90] flex items-center justify-center">
+            <div class="bg-white p-6 rounded-lg shadow-lg max-w-xl w-full">
+                <div class="flex justify-between border-b border-gray-200 pb-2.5">
+                    <h2 class="text-lg font-semibold text-gray-800">Confirmação</h2>
+                    <button @click="openModal = false" class="px-2.5 bg-gray-500 text-white rounded-lg"> X </button>
+                </div>
+                <p class="text-sm text-center text-gray-600 mt-4">
+                    Para prosseguir, marque a caixa abaixo e confirme sua inscrição na lista de plantões extras.
+                </p>
+
+                <div class="flex items-start mt-4">
+                    <input type="checkbox" id="confirmCheckbox" x-model="checked"
+                        class="w-5 h-5 text-[#004B43] border-gray-300 rounded focus:ring-[#42B029]">
+                    <label for="confirmCheckbox" class="ml-2 text-sm text-gray-700">
+                        <p class="text-justify">
+                            Declaro estar ciente do disposto na <strong>Lei nº 6.915, de 28 de novembro de 2022</strong> e manifesto interesse em integrar a lista de plantões extraordinários e eventos extras da Secretaria Municipal de Saúde.
+                        </p>
+                        <p>
+                            <a href="https://leismunicipais.com.br/a/pe/c/caruaru/lei-ordinaria/2022/692/6915/lei-ordinaria-n-6915-2022-institui-e-regulamenta-o-pagamento-de-plantoes-extraordinarios-e-eventos-extras-no-ambito-da-secretaria-da-saude-do-municipio-de-caruaru-e-da-outras-providencias"
+                                class="text-green-700 underline">Acesse a Lei nº 6.915</a>
+                        </p>
+                    </label>
+                </div>
+
+                <div class="flex justify-end space-x-3 mt-6">
+                    <button type="button" class="bg-gray-300 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300" x-show="!checked">
+                        Confirmar
+                    </button>
+
+                    <a href="{{ route('extras.edit', $dbProfessional->id) }}" class="bg-[#42B029] hover:bg-[#004B43] text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300" x-show="checked">
+                        Confirmar
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 
 </body>
